63957dac21197b12995f50b4a992c515dea32b00
ehsanmsz <contact@ehsanmsz.com>
2025-08-18 20:41:38 +0330

    Fix publish before deploy

--- kiwano-log-no-op/build.gradle.kts
+++ kiwano-log-no-op/build.gradle.kts
@@ -1,11 +1,15 @@
 import org.jreleaser.model.Active
+import java.io.FileInputStream
 import java.time.LocalDate
+import java.util.Properties
+import kotlin.apply
 
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jReleaser)
     id("kotlin-parcelize")
+    id("maven-publish")
 }
 
 android {
@@ -40,22 +44,64 @@ dependencies {
 /**
  * Publish
  */
-jreleaser {
-    gitRootSearch.set(true)
-    project {
-        name.set("kiwano-log-no-op")
-        description.set("Ktor client android logger")
-        version.set("0.1.0")
-        license.set("Apache-2.0")
-        author("Ehsan Msz")
-        inceptionYear.set("2024")
-        copyright.set("Copyright (c) ${LocalDate.now().year} Ehsan Msz")
-        links {
-            homepage.set("https://github.com/EhsanMsz/kiwano-log")
-            documentation.set("https://github.com/EhsanMsz/kiwano-log")
-            contact.set("https://ehsanmsz.com")
+val publishVersion = "0.1.0"
+
+val sourceJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("source")
+    from("src/main/java")
+}
+
+artifacts {
+    archives(sourceJar)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.ehsanmsz"
+                artifactId = "kiwano-log-no-op"
+                version = publishVersion
+
+                pom {
+                    name.set(project.name)
+                    description.set("Ktor client android logger")
+                    url.set("https://github.com/EhsanMsz/kiwano-log")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Ehsan Msz")
+                            email.set("contact@ehsanmsz.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:github.com/EhsanMsz/kiwano-log.git")
+                        url.set("https://github.com/EhsanMsz/kiwano-log")
+                        developerConnection.set("scm:git:ssh://git@github.com:EhsanMsz/kiwano-log.git")
+                    }
+                }
+                artifact(sourceJar)
+            }
+        }
+
+        repositories {
+            maven {
+                url = uri(layout.buildDirectory.dir("staging-deploy"))
+            }
         }
     }
+}
+
+jreleaser {
+    gitRootSearch.set(true)
     signing {
         active.set(Active.ALWAYS)
         armored.set(true)
@@ -67,7 +113,6 @@ jreleaser {
             url.set("https://central.sonatype.com/api/v1/publisher")
             sign.set(true)
             sourceJar.set(true)
-            javadocJar.set(true)
             stagingRepository("target/staging-deploy")
         }
     }
--- kiwano-log/build.gradle.kts
+++ kiwano-log/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.jReleaser)
     kotlin(libs.plugins.kotlinSerialization.get().pluginId) version libs.plugins.kotlinSerialization.get().version.toString()
     id("kotlin-parcelize")
+    id("maven-publish")
 }
 
 android {
@@ -75,22 +76,64 @@ dependencies {
 /**
  * Publish
  */
-jreleaser {
-    gitRootSearch.set(true)
-    project {
-        name.set("kiwano-log")
-        description.set("Ktor client android logger")
-        version.set("0.1.0")
-        license.set("Apache-2.0")
-        author("Ehsan Msz")
-        inceptionYear.set("2024")
-        copyright.set("Copyright (c) ${LocalDate.now().year} Ehsan Msz")
-        links {
-            homepage.set("https://github.com/EhsanMsz/kiwano-log")
-            documentation.set("https://github.com/EhsanMsz/kiwano-log")
-            contact.set("https://ehsanmsz.com")
+val publishVersion = "0.1.0"
+
+val sourceJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("source")
+    from("src/main/java")
+}
+
+artifacts {
+    archives(sourceJar)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.ehsanmsz"
+                artifactId = "kiwano-log"
+                version = publishVersion
+
+                pom {
+                    name.set(project.name)
+                    description.set("Ktor client android logger")
+                    url.set("https://github.com/EhsanMsz/kiwano-log")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Ehsan Msz")
+                            email.set("contact@ehsanmsz.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:github.com/EhsanMsz/kiwano-log.git")
+                        url.set("https://github.com/EhsanMsz/kiwano-log")
+                        developerConnection.set("scm:git:ssh://git@github.com:EhsanMsz/kiwano-log.git")
+                    }
+                }
+                artifact(sourceJar)
+            }
+        }
+
+        repositories {
+            maven {
+                url = uri(layout.buildDirectory.dir("staging-deploy"))
+            }
         }
     }
+}
+
+jreleaser {
+    gitRootSearch.set(true)
     signing {
         active.set(Active.ALWAYS)
         armored.set(true)
@@ -102,7 +145,6 @@ jreleaser {
             url.set("https://central.sonatype.com/api/v1/publisher")
             sign.set(true)
             sourceJar.set(true)
-            javadocJar.set(true)
             stagingRepository("target/staging-deploy")
         }
     }
